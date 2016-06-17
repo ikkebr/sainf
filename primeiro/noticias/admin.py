3732ce736e6f1f69829180665212c571809058a6
Henrique Pereira <ikkibr@gmail.com>
2016-06-16 23:36:28 -0300

    Admin melhor para noticias

--- primeiro/noticias/admin.py
+++ primeiro/noticias/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Noticia, Categoria
 
-admin.site.register(Noticia)
+class NoticiaAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'data']
+    list_filter = ['categoria', 'data']
+    search_fields = ['titulo', 'conteudo']
+
+admin.site.register(Noticia, NoticiaAdmin)
 admin.site.register(Categoria)
