68442014c7ad68a1a7b5de1605d1347b5b7dbd5b
Henrique Pereira <ikkibr@gmail.com>
2016-06-16 22:55:05 -0300

    Modelos, admin e settings

--- primeiro/noticias/models.py
+++ primeiro/noticias/models.py
@@ -1,21 +1,25 @@
-from __future__ import unicode_literals
+    from __future__ import unicode_literals
 
-from django.db import models
+    from django.db import models
 
-# Create your models here.
-class Categoria(models.Model):
-    nome = models.CharField(max_length=30)
-    
-    def __unicode__(self):
-        return u"%s" % self.nome
+    # Create your models here.
+    class Categoria(models.Model):
+        nome = models.CharField(max_length=30)
         
+        def __unicode__(self):
+            return u"%s" % self.nome
+            
+
+    class Noticia(models.Model):
+        titulo = models.CharField(max_length=200)
+        data = models.DateTimeField()
+        conteudo = models.TextField()
         
-class Noticia(models.Model):
-    titulo = models.CharField(max_length=200)
-    data = models.DateTimeField()
-    conteudo = models.TextField()
-    
-    categoria = models.ForeignKey(Categoria)
-    
-    def __unicode__(self):
-        return u"%s" % self.conteudo
+        categoria = models.ForeignKey(Categoria)
+        
+        def __unicode__(self):
+            return u"%s" % self.conteudo
+            
+            
+            
+            
