03cf1f0a5e2163c32a41fe45b105bb3e56d18dca
Henrique Pereira <ikkibr@gmail.com>
2016-06-16 23:03:20 -0300

    Update models.py
    Removed extra tabs

--- primeiro/noticias/models.py
+++ primeiro/noticias/models.py
@@ -1,25 +1,21 @@
-    from __future__ import unicode_literals
+from __future__ import unicode_literals
 
-    from django.db import models
+from django.db import models
 
-    # Create your models here.
-    class Categoria(models.Model):
-        nome = models.CharField(max_length=30)
+# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=30)
+    
+    def __unicode__(self):
+        return u"%s" % self.nome
         
-        def __unicode__(self):
-            return u"%s" % self.nome
-            
 
-    class Noticia(models.Model):
-        titulo = models.CharField(max_length=200)
-        data = models.DateTimeField()
-        conteudo = models.TextField()
-        
-        categoria = models.ForeignKey(Categoria)
-        
-        def __unicode__(self):
-            return u"%s" % self.conteudo
-            
-            
-            
-            
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=200)
+    data = models.DateTimeField()
+    conteudo = models.TextField()
+    
+    categoria = models.ForeignKey(Categoria)
+    
+    def __unicode__(self):
+        return u"%s" % self.conteudo
